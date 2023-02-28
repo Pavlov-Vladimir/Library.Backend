2dd3e50b24e26f6e68a6b50fa3fd5d88bb9d391e
Pavlov-Vladimir <bob.pavlov.a7@gmail.com>
2023-02-28 21:45:10 +0200

    Added DataProvider, Service.

---
+++ Library.Business/DataProviders/LibraryDataProvider.cs
@@ -0,0 +1,27 @@
+﻿using Library.Domain.Common.Enums;
+using Library.Domain.Contracts.DataProviders;
+using Library.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Business.DataProviders;
+public class LibraryDataProvider : ILibraryDataProvider
+{
+    public Task<Book> GetBookByIdAsync(int bookId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ICollection<Book>> GetBooksAsync(OrderByProperty? orderBy)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ICollection<Book>> GetRecommendedAsync(string? genre)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Library.Business/Services/LibraryService.cs
@@ -0,0 +1,36 @@
+﻿using Library.Domain.Contracts.Services;
+using Library.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Business.Services;
+public class LibraryService : ILibraryService
+{
+    public Task<int> AddBookAsync(Book book)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddRatingAsync(Rating rating)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> AddReviewAsync(Review review)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteBookAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> UpdateBookAsync(Book book)
+    {
+        throw new NotImplementedException();
+    }
+}
